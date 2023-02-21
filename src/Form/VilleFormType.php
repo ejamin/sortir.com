cb755e2114dcb35ca6ea677f1c29be7bd52fd34a
François <francois.duval.503@gmail.com>
2023-02-21 12:12:40 +0100

    21/02/2023 à 12h10 - Gestion des Villes/Lieux + Token de Suppro

--- src/Form/VilleFormType.php
+++ src/Form/VilleFormType.php
@@ -2,30 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Lieux;
 use App\Entity\Villes;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class LieuxFormType extends AbstractType
+class VilleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', null, ['label' => 'Nom : '])
-            ->add('rue', null, ['label' => 'Rue : '])
-            ->add('latitude', null, ['label' => 'Latitude : '])
-            ->add('longitude', null, ['label' => 'Longitude : '])
+            ->add('nom', null, ['label' => 'Ville : '])
+            ->add('codePostal', null, ['label' => 'Code Postal : '])
+            ->add('ajouter', SubmitType::class, ['label'=> "Ajouter"])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Lieux::class,
+            'data_class' => Villes::class,
             'csrf_protection' => true
         ]);
     }
