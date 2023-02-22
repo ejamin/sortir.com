29c4c7c28096ee710119f87eed09aeda5292989f
Esteban JAMIN <ejamin2022@campus-eni.fr>
2023-02-22 16:43:04 +0100

    ajout gestion des filtres sur page accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,8 @@ use App\Repository\SortiesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\FiltreFormType;
 
 class MainController extends AbstractController
 {   
@@ -19,15 +21,101 @@ class MainController extends AbstractController
     }
 
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $sorties = $this->sortiesRepository->findAll();
-        $sites = $this->sitesRepository->findAll();
+        $sites = $this->sitesRepository->findAll();      
+
+        $defaultData = ['message' => 'Type your message here'];        
+        $form = $this->createForm(FiltreFormType::class, $defaultData);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            $this->filtrerSorties($data,$sorties);
+
+            return $this->render('accueil.html.twig', [
+                "now" => date('d-m-y h:i:s'),
+                "form" => $form->createView(),
+                "sorties" => $sorties,
+                "sites" => $sites,
+                "data" => $data,
+                "user" => $this->getUser()
+            ]);
+        }
 
         return $this->render('accueil.html.twig', [
             "now" => date('d-m-y h:i:s'),
+            "form" => $form->createView(),
             "sorties" => $sorties,
-            "sites" => $sites
+            "sites" => $sites,
+            "data" => $defaultData,
+            "user" => $this->getUser()
         ]);
+    }
+
+    public function filtrerSorties($data, &$sorties){
+        $sortiesFinal = [];
+        foreach($sorties as $sortie){
+            $valide = true;
+            if(isset($data['sites'])){
+                if($sortie->getIdSite()->getID() === $data['sites']->getId()){
+                    $valide = true;
+                }else $valide = false;
+            }
+            if ($valide && isset($data['searchText'])) {
+                if (str_contains($sortie->getNom(), $data['searchText'])) {
+                    $valide = true;
+                }else $valide = false;
+            }
+            if($valide && isset($data['dateMin']) && isset($data['dateMax'])){
+                if ($sortie->getDateDebut() >= $data['dateMin'] && $sortie->getDateDebut() < $data['dateMax']) {
+                    $valide = true;
+                }else $valide = false;
+            }
+            if($valide && isset($data['organisateur'])){
+                if($data['organisateur'] == true){
+                    if($this->getUser()->getID() === $sortie->getIdOrganisateur()->getID()){
+                        $valide = true;                    
+                    }else $valide = false;
+                }
+            }
+            if($valide && isset($data['inscrit'])){
+                if($data['inscrit'] == true){
+                    if($this->existsInArray($this->getUser(), $sortie->getIdParticipant())){
+                        $valide = true;                    
+                    }else $valide = false;
+                }
+            }
+            if($valide && isset($data['pasInscrit'])){
+                if($data['pasInscrit'] == true){
+                    if(!$this->existsInArray($this->getUser(), $sortie->getIdParticipant())){
+                        $valide = true;                    
+                    }else $valide = false;
+                }
+            }
+            if($valide && isset($data['passees'])){
+                if($data['passees'] == true){
+                    if($sortie->getDateDebut() < date('d-m-y h:i:s')){
+                        $valide = true;
+                    }else $valide = false;
+                }
+            }
+
+            if($valide == true){
+                array_push($sortiesFinal,$sortie);                
+            }
+        }
+        $sorties = $sortiesFinal;
+    }
+
+    public function existsInArray($entry, $array) {
+        foreach ($array as $compare) {
+            if ($compare->id == $entry->id) {
+                return true;
+            }
+        }
+        return false;
     }
 }
